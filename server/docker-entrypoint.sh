e9987851f59a3bae614a6823b3108a77335b2a57
FriendlyNGeeks <friendlyneighborhoodgeeks@gmail.com>
2021-08-20 02:06:35 -0400

    unstage

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-npm run test
-# nodemon server.js
+# npm run test
+nodemon server.js
 # /usr/src/server/node_modules/nodemon server.js
 # chmod +x /usr/src/server/node_modules/.bin/nodemon
