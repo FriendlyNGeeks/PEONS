0b74d24c637328a531c7dc73ad8865025058ad14
FriendlyNGeeks <friendlyneighborhoodgeeks@gmail.com>
2021-08-20 02:17:36 -0400

    twofilesupdated

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # npm run test
 # nodemon server.js
-cd /usr/src/server/node_modules/
-ls
-# /usr/src/server/node_modules/nodemon server.js
+# cd /usr/src/server/node_modules/
+# ls
+node_modules/nodemon server.js
 # chmod +x /usr/src/server/node_modules/.bin/nodemon
