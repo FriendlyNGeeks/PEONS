d9a25ed0f5863fa7ac8c0a9ab16f83e9022510d2
FriendlyNGeeks <friendlyneighborhoodgeeks@gmail.com>
2021-08-27 20:28:47 +0000

    usr-src-rescue

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -5,7 +5,10 @@
 # nodemon server.js
 # cd /usr/src/server/node_modules/
 echo "listing dir"
+# cd ..
 ls
+# cd /usr
+# ls
 # node_modules/nodemon server.js
 yarn start
 # chmod +x /usr/src/server/node_modules/.bin/nodemon
